7429a646cb4437402c2363bb55d7e0f299b75386
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-14 17:54:56 -0500

    Create grid for drawing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-
+container.style.gridTemplateColumns = "repeat(4, 1fr)";
 for (let i = 0; i < 16; i++) {
     let box = document.createElement("div");
     box.classList.add("box");
