5fd0bf6376b46b121fc000e956fa1ee48811e1bf
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-14 21:44:08 -0500

    Allow user to select the size of the grid

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const container = document.querySelector(".container");
-container.style.gridTemplateColumns = "repeat(4, 1fr)";
-for (let i = 0; i < 16; i++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    box.addEventListener("mouseover", () => {
-        box.classList.add("hovered");
-    });
-    container.appendChild(box);
+let size = prompt("Enter a grid size.");
+sizeInt = parseInt(size);
+if (sizeInt != NaN && sizeInt <= 100) {
+    creatGrid(size);
+}
+
+
+function creatGrid (gridSize) {
+    const container = document.querySelector(".container");
+    container.style.gridTemplateColumns = "repeat(" + gridSize.toString() + ", 1fr)";
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.addEventListener("mouseover", () => {
+            box.classList.add("hovered");
+        });
+        container.appendChild(box);
+    }
 }
