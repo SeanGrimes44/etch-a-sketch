e337fd4409b754ba9d1afb50001d97ad0b256ec9
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-14 21:57:48 -0500

    Push button to resize the grid

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let size = prompt("Enter a grid size.");
-sizeInt = parseInt(size);
-if (sizeInt != NaN && sizeInt <= 100) {
-    creatGrid(size);
-}
+createGrid(4);
+
+const resizeButton = document.querySelector("button");
+resizeButton.addEventListener("click", () => {
+
+    let child = document.querySelector(".container").lastElementChild;
+    while (child) {
+        document.querySelector(".container").removeChild(child);
+        child = document.querySelector(".container").lastElementChild;
+    }
+    let size = prompt("Enter a grid size.");
+    let sizeInt = parseInt(size);
+    if (sizeInt != NaN && sizeInt <= 100) {
+        createGrid(sizeInt);
+    }
+});
 
 
-function creatGrid (gridSize) {
+function createGrid (gridSize) {
     const container = document.querySelector(".container");
     container.style.gridTemplateColumns = "repeat(" + gridSize.toString() + ", 1fr)";
     for (let i = 0; i < gridSize * gridSize; i++) {
