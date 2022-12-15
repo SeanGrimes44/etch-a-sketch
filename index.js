7a8287f1d6ea0615c12a69767ad2dccbdb7c8c5a
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-14 22:09:24 -0500

    Add constraints to resizing the grid

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 createGrid(4);
 
 const resizeButton = document.querySelector("button");
-resizeButton.addEventListener("click", () => {
+resizeButton.addEventListener("click", resizeGrid);
 
+function resizeGrid () {
     let child = document.querySelector(".container").lastElementChild;
     while (child) {
         document.querySelector(".container").removeChild(child);
         child = document.querySelector(".container").lastElementChild;
     }
-    let size = prompt("Enter a grid size.");
+    let size = prompt("Enter a grid size. Must be a valid number that is less than or equal to 100.");
     let sizeInt = parseInt(size);
     if (sizeInt != NaN && sizeInt <= 100) {
         createGrid(sizeInt);
     }
-});
+    else {
+        resizeGrid();
+    }
+}
 
 
 function createGrid (gridSize) {
